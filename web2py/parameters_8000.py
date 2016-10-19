63804f6f8451aafd443be1d03dee0dd2a348f56a
Sidney- <onelookingformusic@gmail.com>
2016-10-19 10:51:01 -0700

    Added right side bar

--- web2py/parameters_8000.py
+++ web2py/parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$a12bb6f31f8ceed3$f2d1490cc49fa9dc6c187e74f2b7855e7bd76b07"
+password="pbkdf2(1000,20,sha512)$b0678cf9d8f38a8a$4a28f2af9465c4ed94511ecede8a0a9c0f2c1eeb"
