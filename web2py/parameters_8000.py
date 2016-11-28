fdc538d444c1de3a5594b94caf5d4068268830ed
SalvorMallow <brianjones86@gmail.com>
2016-11-27 19:55:19 -0800

    Added database

--- web2py/parameters_8000.py
+++ web2py/parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$b332a03b035cefbc$12d66e890004022c8ccbf470fd8dee5eaa79e9e2"
+password="pbkdf2(1000,20,sha512)$ad9558223a852e6a$fcc9509be430ab8ad131cbf26798c1d0162639cf"
